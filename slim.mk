6b78d3fdaced2f4516762369cb15c2c982099b74
Josue Rivera <prbassplayer@gmail.com>
2014-07-28 23:26:45 +0200

    Honami: Add some more device info flags.
    Change-Id: I4f158a887149ecddc98c937edf9d54f4b048db74
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- slim.mk
+++ slim.mk
@@ -18,7 +18,10 @@ $(call inherit-product, device/sony/honami/full_honami.mk)
 PRODUCT_RELEASE_NAME := honami
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := slim_honami
 PRODUCT_DEVICE := honami
+PRODUCT_NAME := slim_honami
+PRODUCT_BRAND := Xperia
+PRODUCT_MODEL := Z1
+PRODUCT_MANUFACTURER := Sony
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
